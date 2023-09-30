6b75c59f52aae1e99b2fd986801935a74a655ba2
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-09-30 20:09:19 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteryBatches = [4, 5, 3, 6, 8, 2];
+
+const totalBatteries = batteryBatches.reduce((accumulator, currentBatch) => {
+  return accumulator + currentBatch;
+}, 0);
+
+console.log(totalBatteries); // Output: 28
